6146894516e13adb9717b2f142910cfeb0b3c312
Andrea-mr <andrea.minchola@tecsup.edu.pe>
2025-11-07 11:04:33 -0500

    first-commit 7/11/2025

---
+++ scrip.js
@@ -0,0 +1,83 @@
+const form= document.getElementById("todo-form");
+const input= document.getElementById("todo-input");
+const list = document.getElementById("todo-list");
+const clearBtn= document.getElementById("clear-completed");
+const countEl = document.getElementById("count")
+
+let tareas = [];
+
+form.addEventListener("submit", function(e){
+    e.preventDefault();
+    const texto = input.value.trim();
+    if (texto === ''){
+        alert('Por favor, ingrese una tarea');
+        return;
+    }
+
+    const nuevaTarea = {
+        texto: texto,
+        completada: false,
+    };
+
+    tareas.push(nuevaTarea); 
+    input.value='';
+    input.focus();
+    mostrarTareas();
+
+});
+
+function mostrarTareas(){
+    list.innerHTML ='';
+    tareas.forEach ((tarea, index) => {
+        const li = document.createElement("li");
+        li.className = "todo-list";
+        const divIzquierda = document.createElement("div");
+        divIzquierda.className = "todo-left";
+        const check = document.createElement("input")
+        check.type= "checkbox";
+        check.checked =tarea.completada;
+        const texto = document.createElement("span")
+        texto.textContent = tarea.texto;
+        texto.className="todo-title";
+        if (tarea.completada){
+            texto.classList.add("Completed");
+        }
+
+        check.addEventListener("change",()=>{
+            tarea.completada = check.checked;
+            mostrarTareas();
+        });
+        
+        divIzq.appendChild(check);
+        divIzq.appendChild(texto);
+
+        const divDer = document.createElement("div");
+        divDer.className = "todo-actions";
+
+        const btnEliminar = document.createElement("button");
+        btnEliminar.textContent = " "
+        btnEliminar.addEventListener("click", ()=> {
+            tareas.splice(index,1);
+            mostrarTareas();
+    });
+
+        divDer.appendChild(btnEliminar);
+
+        li.appendChild(divIzq)
+        li.appendChild(divDer)
+
+        list.appendChild(li);
+    });
+    
+    actualizarContador();
+}
+
+clearBtn.addEventListener("click",()=> {
+    tareas = tareas.filter((t) => !t.completada);
+    mostrarTareas();
+});
+
+function actualizarContador(){
+    const pendientes = tareas.filter((t) => !t.completada).length;
+    countEl.textContent = pendientes + (pendientes === 1 ? "tarea pendiente" : "tareas pendientes");
+}
